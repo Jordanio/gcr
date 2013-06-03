11440e190eec32e713229d939b98840a38ef367c
mproy <michaelproy@gmail.com>
2013-06-03 13:41:41 -0400

    first commit to local GCR repo

---
+++ _assets/js/script.js
@@ -0,0 +1,32 @@
+        
+        $(function() {
+            var pull        = $('#pull');
+                menu        = $('nav ul');
+                menuHeight  = menu.height();
+               
+
+            $(pull).on('click', function(e) {
+                e.preventDefault();
+                menu.slideToggle();
+            });
+
+/*
+            $(window).resize(function(){
+                var w = $(window).width();
+                if(w < 800 && menu.is(':hidden')) {
+                    menu.removeAttr('style');                    
+                } 
+                
+            });
+            
+            
+            if (!(pull.is(':hidden'))) 
+                menu.slideToggle();
+            }
+   
+   */
+            
+            
+            
+            
+        });
